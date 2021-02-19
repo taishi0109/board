95347fa8fc73f26396d727f8eb743e4c020231b1
taishi9 <taishi0109@gmail.com>
2021-02-19 19:33:35 +0900

    コメントアウト削除

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,3 @@
 class Like < ApplicationRecord
   belongs_to :post
-  # has_many :likes,dependent: :destroy
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   root 'topics#index'
   resources :topics, only: [:index, :show, :new, :create, :destroy] do
     resources :posts, only: [:show, :edit, :update, :create], module: :topics do
-      # resources :likes, only: [:create]
       post :likes, to: 'posts/likes#create'
 
       collection do
@@ -10,9 +9,4 @@ Rails.application.routes.draw do
       end
     end
   end
-  # resources :posts do
-  #   resources :likes, only: [:create]
-  # end
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
